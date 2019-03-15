3dff205d90c98bc8ddfb1f94d2995cf40bdfbd0c
fkckfc <fkcheng89@gmail.com>
2019-03-15 16:35:32 +0800

    Add files via upload

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,1 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DynamicProgramming dp = new DynamicProgramming();
+            //SlidingWindow sw = new SlidingWindow();
+            WaterContainer wc = new WaterContainer();
+        }
+    }
+}
